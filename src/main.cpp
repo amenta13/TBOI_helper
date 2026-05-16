d527533bc60743675c243d798d188ce2b5c2946f
amenta13 <andrewgsoccer2004@gmail.com>
2026-05-16 19:49:58 -0400

    Added basic user input

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,8 @@
 
 int main(){
     
-    std::cout << "Working directory: " << std::filesystem::current_path() << "\n";
+    //std::cout << "Working directory: " << std::filesystem::current_path() << "\n";    // DEBUG
+
     ItemDatabase db;
     if (!db.loadFromCSV("data/isaac_items.csv")) {
         std::cout << "Failed to load CSV\n";
@@ -16,17 +17,37 @@ int main(){
     }
     std::cout << "Loaded " << db.byId.size() << " items\n";
 
-    // Test lookup by ID
-    const Item& it = db.getById(1);
-    std::cout << "ID 1: " << it.name << "\n";
-    std::cout << "Quality: " << it.quality << "\n";
-    std::cout << "Types:\n";
-    for (const auto& t : it.type)
-        std::cout << " - " << t << "\n";
+    // Get user input
+    int userID;
+    std::cout << "Enter Item ID: ";
+    std::cin >> userID;
 
-    // Test lookup by name
-    const Item& it2 = db.getByName("The Sad Onion");
-    std::cout << "\nLookup by name: " << it2.name << "\n";
+    // Search for userID
+    const Item& it = db.getById(userID);
+
+    // Return the item with the userID and its characteristics
+    std::cout << "ID: " << it.id << "\n";
+    std::cout << "Name: " << it.name << "\n";
+    std::cout << "Types:\n";
+    for (const auto& a : it.type)
+        std::cout << " - " << a << "\n";
+
+    // Lookup by name
+    // Get user input
+    std::string user_str;
+    std::cout << "Enter Item Name: ";
+    std::cin >> user_str;
+
+    // Search for user_str
+    const Item& it2 = db.getByName(user_str);
+
+    // Return the item with the user_str and its characteristics
+    std::cout << "ID: " << it2.id << "\n";
+    std::cout << "Name: " << it2.name << "\n";
+    std::cout << "Quality: " << it2.quality << "\n";
+    std::cout << "Types:\n";
+    for (const auto& b : it2.type)
+        std::cout << " - " << b << "\n";
 
     return 0;
 }
